5bd77438c6124101152c0b751cf288abd20c0869
Abbosbek Yuldoshev Muzaffarovich <abbosbekyuldoshevmuzaffarovich@Abbosbeks-MacBook-Pro.local>
2023-02-26 01:01:46 +0900

    first commit

---
+++ src/state.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+class Student extends React.Component{
+    render (){
+        
+        return (
+            <div>
+            <h1></h1>
+            </div>
+            );
+
+    }
+}
+
+
+
+
+
+
+export default Student;

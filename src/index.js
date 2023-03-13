3a0710988e8dde39706af209a5a735a5f191a25f
Abbosbek Yuldoshev Muzaffarovich <abbosbekyuldoshevmuzaffarovich@Abbosbeks-MacBook-Pro.local>
2023-03-14 00:19:59 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import State  from "./state"
+import Student from "./state"
 
 
 const webbrain = ReactDOM.createRoot(document.getElementById("root"));
 webbrain.render(
   <React.StrictMode>
-    <State/>
+    <Student/>
     </React.StrictMode>
 )
 

1b618034a839e5848bb9e291c23af5f7a87923e7
enraldo0923 <91226483+enraldo0923@users.noreply.github.com>
2023-03-20 09:56:17 +0900

    project configrated

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter,Navigate,Route,Routes } from "react-router-dom";
+import { navbar } from "../utils/navbar";
 export const Root =()=>{
-   return <BrowserRouter>
+   return (
+   <BrowserRouter>
    <Routes>
-    <Route path ="home" element ={}/>
-    <Route path ="proporties"/>
-    <Route path ="home"/>
+    {navbar.map(({path,element})=>{
+    return <Route path={path} element={element}/>;
+        })};
+    <Route path ="*" element ={<h1>404 NOT FOUND</h1>}/>
+    <Route path ="/" element ={<Navigate to ={"/home"}/>}/>
    </Routes>
    </BrowserRouter>
+
+   );
 };
 export default Root;
--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -1,1 +1,16 @@
-export const navbar =[];
+import useUniqueId from "../Hooks/useId";
+import HomePage from " ../pages/Home";
+import Proporties from " ../pages/Proporties";
+
+
+
+export const navbar =[
+
+    {
+        id: useUniqueId,element :  <HomePage/> , title :"Home",path: "/home",private : false , hidden: false
+    },
+    {
+        id: useUniqueId,element : <Proporties/>, title :"Proporties",path: "/Proporties",private : false , hidden: false
+    }
+];
+

54a959571b6adaadc929ecc9126d4c30e235897f
enraldo0923 <91226483+enraldo0923@users.noreply.github.com>
2023-05-02 10:21:48 +0900

    crud

--- src/state.jsx
+++ src/state.jsx
@@ -1,57 +1,157 @@
-import React from "react";
-import {student} from './student';
 
+import React from "react";
+import {student} from "./student";
 
 class State extends React.Component{
+
     constructor(props){
         super(props);
-        this.state ={
+        this.state={
             data: student,
-            name:  "Abbosbek",
-            surname: 'Yuldoshev',
-        }
-    }
-    render(){
-       
-       const plus =()=>{
-               if(this.state.count < 10){
-               this.setState({count  : this.state.count + 1 })
-            };
+            name : '',
+            status : "",
         };
-        const minus =()=>{
-            if(this.state.count > 1){
-            this.setState({count : this.state.count -1})
-        };
-        };
-        const onChange =(event)=>{
-            this.setState({surname: event.target.value})
-        }
-        // const onChangeName =(event)=>{
-        //     console.log(event.target.value);
-        //     this.setState({surname: event.target.value})
-        // }
-        return (
-            <div>
-                <h1>Name : {this.state.name}</h1>
-                <h1>SurName : {this.state.surname}</h1>
-                <input type= "name" onChange={onChange}  placeholder="Name"/>
-                <input type= "surname" onChange={onChange}  placeholder="SurName"/>
-                <h1>{this.state.count}</h1>
-                <hr />
-                <button onClick={plus}>+++++</button>
-                
-                <hr />
-                <button onClick={minus}>---------</button>
-                {
-                this.state.data.map(({id, name ,status})=>{
-                    return(
-                        <h1 key={id}>{id} {name}{status}</h1>
-                    )
-                })
-                }
-            </div>
-        )
     };
+    render(){
+        const onChange=(event)=>{
+            console.log(event.target.value);
+            this.setState({[event.target.name]: event.target.value});
+        }
+         const onFilter=(abbos)=>{
+            console.log(abbos.target.value);
+            let res = student.filter((value)=>value.name.includes(abbos.target.value));
+            this.setState({
+                data: res
+            });
+         };
+        const onAdd =()=>{
+            let user = {id:Date.now(),name:this.state.name,status: this.state.status};
+            this.setState({
+                data :[... this.state.data, user],
+                name: "",
+                status :"",
+            })
+            console.log(user);
+
+        }
+         const onDelete=(id)=>{
+            console.log(id);
+            let res = this.state.data.filter(value=>value.id !==id)
+            this.setState({
+                data :res,
+            })
+         }
+         return(
+            <div>
+                <h1>Name :{this.state.name}</h1>
+                <h1>Status :{this.state.status}</h1>
+                <input value={this.state.name} name="name" onChange={onChange} type="text" placeholder="Name" />
+                <input value={this.state.status} name="status" onChange={onChange} type="text" placeholder="Status" />
+                <button onClick={onAdd}>Add</button>
+                <input onChange={onFilter} type="text"placeholder="Filter" />
+                <table border={'1px'} width={'50%'}>
+                    <thead>
+                      <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Delete</th>
+                        <th>Edit</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        {
+                        this.state.data.map(({id,name,status})=>{
+                            return(
+                                <tr key={id}>
+                                    <td>{id}</td>
+                                    <td>{name}</td>
+                                    <td>{status}</td>
+                                    <td><button onClick={()=>onDelete(id)}>delete</button></td>
+                                    <td><button>edit</button></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+         )
+    }
 };
 export default State;
-    
+// import React from "react";
+// import {student} from "./student"
+
+// class State extends React.Component{
+
+//   constructor(props){
+//     super(props);
+//     this.state={
+//       data: student,
+//       name : "",
+//       status: "",
+//     };
+//   };
+//    render(){
+
+//      const onChange=(abbos)=>{
+//       console.log(abbos.target.value);
+//       this.setState({[abbos.target.name]:abbos.target.value });
+
+//      };
+
+//      const onFilter=(asad)=>{
+//      console.log(asad.target.value);
+//      let res = student.filter((value)=> value.name.includes(asad.target.value))
+//      this.setState({
+//       data: res,})};
+
+//      const onAdd = ()=>{
+//      let user = {id: Date.now(),name : this.state.name , status: this.state.status};
+//      this.setState({
+//       data :[...this.state.data,user],
+//       name: "",
+//       status:""
+//      });
+//      console.log(user);};
+
+//      return(
+//       <div>
+//         <h1>Name: {this.state.name}</h1>
+//         <h1>Status: {this.state.status}</h1>
+//         <input  onChange={onChange} value={this.state.name} name="Name" placeholder="Name" type="text" />
+//         <input  onChange={onChange} value={this.state.status} name="Status" placeholder="Status" type="text" />
+//         <button onClick={onAdd}>Add</button>
+//         <br />
+//         <br />
+//         <input onChange={onFilter} placeholder="Filter" type="text" />
+//         <table border={1} width={'50%'}>
+//           <thead>
+//             <tr>
+//               <th>Id</th>
+//               <th>Name</th>
+//               <th>Status</th>
+//               <th>Delete</th>
+//               <th>Edit</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {this.state.data.map(({id,name,status})=>{
+//               return (
+//                 <tr key={id}>
+//                   <td>{id}</td>
+//                   <td>{name}</td>
+//                   <td>{status}</td>
+//                   <td><button>Delete</button></td>
+//                   <td><button>Edit</button></td>
+
+//                 </tr>
+//               )
+//             })}
+//           </tbody>
+//         </table>
+//       </div>
+//      )
+//    }
+// };
+// export default State ;
--- src/student.js
+++ src/student.js
@@ -1,12 +1,23 @@
 
 
 
-
 export const student = [
-    {id: 14, name :'Abbosbek', status: 'It',},
-    {id: 26, name :'Asadbek', status: 'job',},
-    {id: 37, name :'Azamatbek', status: 'work',},
-    {id: 42, name :'Muzaffar', status: 'doctor',},
-    {id: 51, name :'Gulbahor', status: 'business',},
-    {id: 68, name :'Eshqobil', status: 'uy',},
+    {id: 1, name :'Abbosbek', status: ' It',},
+    {id: 2, name :'Asadbek', status: ' job',},
+    {id: 3, name :'Azamatbek', status: ' work',},
+    {id: 4, name :'Muzaffar', status: ' doctor',},
+    {id: 5, name :'Gulbahor', status: ' business',},
+    {id: 6, name :'Eshqobil', status: ' uy',},
+    {id: 7, name :'Zafar', status: 'Za',},
+    {id: 8, name :'Hasan', status: ' Ha',},
+    {id: 9, name :'Akmal', status: ' Ak',},
+    {id: 10, name :'Eshmat', status: ' Esh',},
+    {id: 11, name :'Toshmat', status: ' To',},
+    {id: 12, name :'Ergash', status: ' Er',},
+    {id: 13, name :'Qobil', status: 'Qo',},
+    {id: 14, name :'Ahmad', status: 'Ah',},
+    {id: 15, name :'Umar', status: ' Um',},
+    {id: 16, name :'Kumar', status: ' Ku',},
+    {id: 17, name :'Bahtiyor', status: ' Ba',},
+    {id: 18, name :'Ixtiyor', status: 'Ix',},
 ]

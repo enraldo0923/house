cc8a1d67750402575207f03eb86ad010fa8d59a8
enraldo0923 <91226483+enraldo0923@users.noreply.github.com>
2023-05-07 11:15:19 +0900

    styled-components

--- src/state.jsx
+++ src/state.jsx
@@ -1,36 +1,45 @@
+// // Styled-Components -1 
+
 
 // import React from "react";
-// import { Container , Box, Title,Des} from "./style";
+// import { Container ,Box,Title,Title2,Title3} from "./style";
 
 
 
+//   class State extends React.Component{
+
+//     constructor(props){
+//       super(props);
+//       this.state={}
+//     };
+//     render(){
+//       return(
+//         <div>
+//         <Container>Abbosbek</Container>
+//         <Box size="large">Large</Box>
+//         <Box size="medium">Medium </Box>
+//         <Box size="small">Small</Box>
+//         <Title>Abbosbek</Title>
+//         <Title2>Asadbek</Title2>
+//         <Title3>Azamatbek</Title3>
+//         </div>
+//       )
+//     }
+//   };
+//   export default State;
 
 
-
-// class State extends React.Component{
-
-  
-//   render(){
-//     return(
-     
-//        <Container>
-//         <Box type="large">Large</Box>
-//         <Box type="medium">Medium</Box>
-//         <Box type="small">Small</Box>
-//         <Title>Hey</Title>
-//         <Des>Men</Des>
-//        </Container>
-  
-      
-                    
-//     )
-//   }
-// }
-// export default State;
+// Styled-Components -2;
 
 
 import React from "react";
-import { Container ,Box,Title,Title2,Title3} from "./style";
+import {Container, Button,ActiveButton,Rotate } from "./style";
+import {ThemeProvider,createGlobalStyle} from "styled-components"
+
+
+const GlobalStyle = createGlobalStyle`
+  
+`
 
 
 
@@ -42,17 +51,11 @@ import { Container ,Box,Title,Title2,Title3} from "./style";
     };
     render(){
       return(
-        <div>
-        <Container>Abbosbek</Container>
-        <Box size="large">Large</Box>
-        <Box size="medium">Medium </Box>
-        <Box size="small">Small</Box>
-        <Title>Abbosbek</Title>
-        <Title2>Asadbek</Title2>
-        <Title3>Azamatbek</Title3>
-        </div>
-      )
-    }
-  };
-  export default State;
+        <div><Container>
 
+          <Button>Abbosbek</Button>
+          <ActiveButton>Active Button</ActiveButton>
+          <Rotate>Rotate</Rotate>
+        </Container>
+        </div>)}};
+        export default State;
--- src/style.js
+++ src/style.js
@@ -1,108 +1,125 @@
-// import styled,{css} from "styled-components";
+// // Styled-Components -1
+
+
+// import styled, {css} from "styled-components";
 
 
 
-
-// export const Container = styled.div`
-
-// background-color: orange;
-// display: flex;
-// color: #000;
-// width: 100%;height: 400px;
-
-// `;
-
-
-// const getSize =(props)=>{
-//     switch(props.type){
-//      case 'large': return "300px";
-//      case 'medium': return "200px";
-//      case 'small': return "100px";
-//      default :return "100px"
-//     }
-
-// }
-// export const Box  = styled.div`
-// display: flex;
-// width: ${getSize};
-// height:${getSize};
-// border:2px solid blue;
-// margin: 10px;
-// align-items: center;
-// justify-content: center;
-// `;
-// export const Title = styled.h1`
-// color: black;
-// font-weight: 400;
-// padding: 10px;
-// border: 4px solid black;
-// height: fit-content;
-
-// `
-// export const Des = styled.h1`
-// color: black;
-// font-weight: 400;
-// padding: 10px;
-// border: 4px solid black;
-// height: fit-content;
-
-// `
-
-
-import styled, {css} from "styled-components";
-
-
-const OnWidth=(props)=>{
-  switch (props.size) {
-    case 'large': return "300px";
-    case 'medium': return "200px";
-    case 'small': return "100px";
-    default : return "200px"
+// const OnWidth=(props)=>{
+//   switch (props.size) {
+//     case 'large': return "300px";
+//     case 'medium': return "200px";
+//     case 'small': return "100px";
+//     default : return "200px"
         
         
   
    
-  }
-}
+//   }
+// }
 
+// export const Container = styled.div`
+// width: 200px;
+// height: 200px;
+// display: flex;
+// background-color: red;
+// color: #000;
+// margin: 20px;
+// padding: 40px;
+// justify-content: space-around;
+// font-family: 'Times New Roman', Times, serif;
+
+// `
+// export const Box = styled.div`
+// width:${(OnWidth)} ;
+// height: ${(OnWidth)};
+// display: flex;
+// background-color: red;
+// color: #000;
+// margin: 20px;
+// padding: 40px;
+// justify-content: space-around;
+// font-family: 'Times New Roman', Times, serif;
+// `
+// const Common = css`
+// border-radius: 2px;
+// border: 2px, solid,red;
+// background-color: aliceblue;
+// color: aqua;
+// `;
+// export const Title = styled.h1`
+// ${Common};
+// font-size: 20px;
+// `
+// export const Title2 = styled.h1`
+// ${Common};
+// font-size: 40px;
+// `
+// export const Title3 = styled.h1`
+// ${Common};
+// font-size: 60px;
+// `;
+
+
+// Styled-Components -2
+
+
+import styled, {keyframes} from "styled-components";
+
+const rotate = keyframes`
+from{
+    transform: rotate(0deg);
+}
+to{
+    transform: rotate(360deg);   
+}
+`;
+export const Rotate = styled.div`
+width: 100px;
+height: 100px;
+border-radius:50%;
+border: 2px solid coral; 
+animation: ${rotate} 2s linear infinite;
+align-items: center;
+display: flex;
+justify-content: center;
+
+`;
+
+export const Button = styled.div`
+margin-left: 40px;
+width: 200px;
+height: 40px;
+background: coral;
+border-radius: 8px;
+display: flex;
+align-items: center;
+justify-content: center;
+color: #000;
+cursor: pointer;
+:active{
+    transform: scale(0.97);
+    opacity: 0.7;
+
+}
+`;
 export const Container = styled.div`
 width: 200px;
 height: 200px;
 display: flex;
 background-color: red;
 color: #000;
-margin: 20px;
+margin: 50px;
 padding: 40px;
-justify-content: space-around;
-font-family: 'Times New Roman', Times, serif;
 
-`
-export const Box = styled.div`
-width:${(OnWidth)} ;
-height: ${(OnWidth)};
-display: flex;
-background-color: red;
-color: #000;
-margin: 20px;
-padding: 40px;
-justify-content: space-around;
 font-family: 'Times New Roman', Times, serif;
-`
-const Common = css`
-border-radius: 2px;
-border: 2px, solid,red;
-background-color: aliceblue;
-color: aqua;
+align-items: center;
+display: flex;
+justify-content: center;
 `;
-export const Title = styled.h1`
-${Common};
-font-size: 20px;
+export const ActiveButton = styled(Button)`
+width: 300px;
+height: 60px;
+margin-left: 20px;
 `
-export const Title2 = styled.h1`
-${Common};
-font-size: 40px;
-`
-export const Title3 = styled.h1`
-${Common};
-font-size: 60px;
-`;
+

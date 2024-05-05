7afe7527c164f4dba4e6e98f92892c67dfb21372
enraldo0923 <91226483+enraldo0923@users.noreply.github.com>
2024-05-05 14:49:21 +0900

    styled-components

--- src/stateStyle.js
+++ src/stateStyle.js
@@ -1,62 +1,36 @@
-import { css } from "styled-components";
+import { css ,keyframes} from "styled-components";
 import styled from 'styled-components';
+export const Container= styled.div`
+margin: 20px;`
 
-
-export const Container = styled('div')`
-display:flex;
-background-color: coral;
-`;
 
 export const Button = styled.div`
 display: flex;
-cursor: pointer;
-:active{
-    transform: scale(0.95);
-};
-border-radius:8px;
- width: 200px;
- height: 40px;
- border:2px solid black;
- margin: 10px;
- align-items: center;
- justify-content:center;`;
-
- const getSize =(props)=>{
-    switch (props.type) {
-        case 'small': return '200px'
-        case 'big': return '300px'
-        case 'medium': return '250px'
-        default: return '150px'
-
-    }
- }
-
-
-export const Box = styled.div`
-
-width: ${getSize};
-height: ${getSize};
-border: 2px solid blue;
-margin: 10px;
-display: flex;
-justify-content: center;
 align-items: center;
-font-size: 40px;
+justify-content: center;
+width: 250px;
+margin: 20px;
+height: 200px;
+:active{
+    transform: scale(0.93);
+}
+background-color: coral;
+border-radius: 20%;
 `;
 
-const common = css`
-color:blue;
-font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-font-weight: 400;
-padding: 10px;
-border:1px solid black;
-height:fit-content;
-`
 
-export const Title = styled.h1`
-${common};`
-
-
-export const Des = styled.h1`
-${common};
-font-size:16px;`
+const rotate= keyframes`
+from{
+    transform: rotate(0deg);
+}to {
+    transform : rotate(360deg);
+}`;
+export const Rotate = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 200px;
+height: 200px;
+border-radius:50%;
+border:2px solid coral;
+animation:${rotate} 2s linear infinite;`
